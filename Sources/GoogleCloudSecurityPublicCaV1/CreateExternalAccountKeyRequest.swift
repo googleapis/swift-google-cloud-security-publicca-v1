9aca0e94a28736b4d6458d1a9b50c0e17a122e52
coryan <coryan@google.com>
2026-04-02 16:06:53 -0400

    impl(publicca-v1): initial messages (#13)
    Hand-craft some code in what will become the generated client. This is
    to guide the generator implementation.

---
+++ Sources/GoogleCloudSecurityPublicCaV1/CreateExternalAccountKeyRequest.swift
@@ -0,0 +1,35 @@
+// Copyright 2026 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// Creates a new
+/// [ExternalAccountKey][google.cloud.security.publicca.v1.ExternalAccountKey] in
+/// a given project.
+///
+/// [google.cloud.security.publicca.v1.ExternalAccountKey]: `ExternalAccountKey`
+public struct CreateExternalAccountKeyRequest: Codable, Equatable, CustomDebugStringConvertible {
+  /// Required. The parent resource where this external_account_key will be
+  /// created. Format: projects/\[project_id\]/locations/\[location\]. At present
+  /// only the "global" location is supported.
+  public let parent: String
+
+  /// Required. The external account key to create. This field only exists to
+  /// future-proof the API. At present, all fields in ExternalAccountKey are
+  /// output only and all values are ignored. For the purpose of the
+  /// CreateExternalAccountKeyRequest, set it to a default/empty value.
+  public let externalAccountKey: ExternalAccountKey?
+
+  public var debugDescription: String {
+    "CreateExternalAccountKeyRequest(parent: \(parent), externalAccountKey: \(externalAccountKey?.debugDescription ?? "nil"))"
+  }
+}
